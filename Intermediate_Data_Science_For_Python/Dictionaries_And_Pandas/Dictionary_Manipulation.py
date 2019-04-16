87bc95cbeaacaaf722713c3df7857ff5dd4858f2
Aditya Mankare <adityamankare@gmail.com>
2019-04-16 18:57:02 -0500

    Create Dictionary_Manipulation.py

---
+++ Intermediate_Data_Science_For_Python/Dictionaries_And_Pandas/Dictionary_Manipulation.py
@@ -0,0 +1,20 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] = 'rome' 
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+del(europe['australia'])
+
+# Print europe
+print(europe)
