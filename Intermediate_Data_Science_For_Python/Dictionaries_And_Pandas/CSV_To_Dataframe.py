35583db0e50217d142c049971dcb827c7a022f2b
Aditya Mankare <adityamankare@gmail.com>
2019-04-16 19:05:40 -0500

    Update CSV_To_Dataframe.py

--- Intermediate_Data_Science_For_Python/Dictionaries_And_Pandas/CSV_To_Dataframe.py
+++ Intermediate_Data_Science_For_Python/Dictionaries_And_Pandas/CSV_To_Dataframe.py
@@ -1,4 +1,9 @@
-#CSV to DataFrame (2)
+#CSV to DataFrame
+
+#To import CSV data into Python as a Pandas DataFrame you can use read_csv().
+
+#Let's explore this function with the same cars data from the previous exercises. This time, however, the data is available in a CSV file, named cars.csv. It is available in your current working directory, so the path to the file is simply 'cars.csv'.
+
 #Your read_csv() call to import the CSV data didn't generate an error, but the output is not entirely what we wanted. The row labels were imported as another column without a name.
 
 #Remember index_col, an argument of read_csv(), that you can use to specify which column in the CSV file should be used as a row label? Well, that's exactly what you need here!
