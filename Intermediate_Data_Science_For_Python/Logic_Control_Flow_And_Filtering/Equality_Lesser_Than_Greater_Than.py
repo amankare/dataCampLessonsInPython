6c4ed857a4005a4d601c36228a66ca38ea9f1336
Aditya Mankare <adityamankare@gmail.com>
2019-04-16 19:18:46 -0500

    Create Equality_Lesser_Than_Greater_Than.py

---
+++ Intermediate_Data_Science_For_Python/Logic_Control_Flow_And_Filtering/Equality_Lesser_Than_Greater_Than.py
@@ -0,0 +1,36 @@
+#Equality
+
+#To check if two Python values, or variables, are equal you can use ==. To check for inequality, you need !=
+
+_____
+
+# Comparison of booleans
+print('True' == 'False')
+
+# Comparison of integers
+print(((-5)*15) != 75)
+
+# Comparison of strings
+print("pyscript" == "PyScript")
+
+# Compare a boolean with an integer
+print(True == 1)
+
+_____
+
+#Greater and less than
+
+#In the video, Filip also talked about the less than and greater than signs, < and > in Python. You can combine them with an equals sign: <= and >=. Pay attention: <= is valid syntax, but =< is not.
+
+_____
+
+# Comparison of integers
+x = -3 * 6
+print(x >= (-10))
+
+# Comparison of strings
+y = "test"
+print("test" <= y)
+
+# Comparison of booleans
+print(True > False)
